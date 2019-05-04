31c5b636782e05c2ec83d42bbea6ff3a6f70e652
Liam Kirsh <liam.kirsh@gmail.com>
2019-05-03 21:44:31 -0400

    fix drop downs on what you can do

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,5 +3,6 @@ var accordions = document.querySelectorAll('.accordion-item');
 for (var i = 0; i < accordions.length; i++) {
     accordions[i].addEventListener('click', function(e) {
 		this.querySelector('.accordion-item__content').classList.toggle('is-open');
+        this.querySelector('.accordion-item__title').classList.toggle('is-open');
     });
 }
